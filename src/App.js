0c04a80f86803ecf6b3f27033f937afac7532d2a
Dan <danneil_teng@yahoo.com>
2023-01-09 02:32:32 +0800

    Add edit, delete and history functions

--- src/App.js
+++ src/App.js
@@ -1,53 +1,101 @@
 import { useState } from "react";
 import "./App.css";
-import TodoList from "./components/TodoList";
 import { VscTrash } from "react-icons/vsc";
 import { FiEdit2 } from "react-icons/fi";
-import { HiSearch } from "react-icons/hi";
+import { HiSearch, HiOutlinePlusCircle } from "react-icons/hi";
+import { TiTick } from "react-icons/ti";
+import { TodoEG } from "./Example";
 
 function App() {
   const [newTodo, setTodo] = useState("");
   const [items, setItems] = useState([]);
-  const [toggle, setToggle] = useState(false);
+  const [toggleH, setToggleH] = useState(false);
+  const [toggleS, setToggleS] = useState(false);
+  const [toggleE, setToggleE] = useState(false);
+  const [search, setSearch] = useState("no results");
+  const [edit, setEdit] = useState(false);
+  const [id, setId] = useState("");
+  const [value, setValue] = useState("");
 
-  function addTodo(e) {
+  function onSubmit(e) {
     e.preventDefault(); //prevents page from refreshing
 
     if (!newTodo) return;
+    if (toggleE) {
+      let tempItems = items.map((item) => {
+        return item.id === id ? { ...item, id, value } : item; //update object/array
+      });
+      console.log(items);
+      setItems(tempItems);
+      console.log(items);
+      setToggleE(false);
+    } else {
+      const item = {
+        id: Math.floor(Math.random() * 10000), //not foolproof to making a unique id
+        value: newTodo,
+      };
+      setItems((oldList) => [...oldList, item]);
+    }
 
-    const item = {
-      id: Math.floor(Math.random() * 10000), //not foolproof to making a unique id
-      value: newTodo,
-    };
-
-    setItems((oldList) => [...oldList, item]);
     setTodo("");
   }
 
-  function deleteTodo(id) {
-    const newArray = items.filter((item) => item.id !== id);
+  function deleteTodo(selected) {
+    const newArray = items.filter((item) => item.selected !== selected);
     setItems(newArray);
   }
 
-  function editTodo(id) {
-    console.log(items);
+  function editTodo(selected) {
+    setToggleE(!toggleE);
+    setTodo(selected.value); //set input
+
+    const editedArray = items.filter((item) => item.id !== selected.id);
+    const selectedArray = items.find((item) => item.id === selected.id);
+    console.log(selectedArray);
+    let { id, value } = selectedArray;
+    setId(id);
+    setValue(value);
+    setToggleE(true);
+    console.log(id);
+    console.log(value);
+    // setItems(editedArray);
+    // setItems((oldList) => [...oldList, selectedItem]);
+
+    // console.log(editedArray);
+    // console.log(items);
   }
 
   function toggled() {
-    if (toggle === false) {
-      setToggle(true);
-    } else setToggle(false);
+    if (toggleH === false) {
+      setToggleH(true);
+    } else setToggleH(false);
   }
 
-  function searchTodo() {}
+  function inputChange(e) {
+    setTodo(e.target.value);
+    setSearch(e.target.value);
+    setValue(e.target.value);
+  }
+
+  function searchTodo(e) {
+    setToggleS(!toggleS);
+    TodoEG.filter((eg) => eg.value.toLowerCase().includes(search));
+    //   const arraySearch = items.filter((item) => {
+    //     return e.target.value;
+    //   });
+    //   console.log(arraySearch);
+    //   if (toggleS === false) {
+    //     setToggleS(true);
+    //   } else setToggleS(false);
+  }
 
   return (
     <div className="todo-app">
       <h1>Todo app</h1>
       <form
         className="todo-form"
-        onSubmit={addTodo}
-        autoComplete={toggle === false ? "off" : "on"}
+        onSubmit={onSubmit}
+        autoComplete={toggleH === false ? "off" : "on"}
       >
         <input
           autoFocus
@@ -55,15 +103,31 @@ function App() {
           name="text"
           value={newTodo}
           className="todo-input"
-          onChange={(e) => setTodo(e.target.value)} //enables typing
+          onChange={(e) => inputChange(e)} //enables typing
         />
-        <button className="todo-button" type="submit">
-          Add
-        </button>
-        <button className="toggle-button" onClick={toggled}>
+        {!toggleS ? (
+          <button
+            className={!toggleE ? "add-button" : "addEdit-button"}
+            type="submit"
+          >
+            {!toggleE ? (
+              <>
+                Add <HiOutlinePlusCircle />
+              </>
+            ) : (
+              <>
+                Edit <TiTick />
+              </>
+            )}
+          </button>
+        ) : (
+          <></>
+        )}
+
+        <button className="toggle-button" type="button" onClick={toggled}>
           History
         </button>
-        <button className="search-button" onClick={searchTodo}>
+        <button className="search-button" type="button" onClick={searchTodo}>
           <HiSearch />
         </button>
 
@@ -74,12 +138,14 @@ function App() {
                 {item.value}
                 <button
                   className="edit-button"
-                  onClick={() => editTodo(item.id)}
+                  type="button"
+                  onClick={() => editTodo(item)}
                 >
                   <FiEdit2 />
                 </button>
                 <button
                   className="delete-button"
+                  type="button"
                   onClick={() => deleteTodo(item.id)}
                 >
                   <VscTrash />
@@ -88,6 +154,13 @@ function App() {
             );
           })}
         </ul>
+        {/* <ul>
+          {TodoEG.filter((eg) => eg.value.toLowerCase().includes(search)).map(
+            (eg) => (
+              <li key={eg.id}>{eg.value}</li>
+            )
+          )}
+        </ul> */}
       </form>
     </div>
   );
---
+++ src/Example.js
@@ -0,0 +1,14 @@
+export const TodoEG = [
+  {
+    id: "1",
+    value: "Get ready",
+  },
+  {
+    id: "2",
+    value: "Leave house to eat",
+  },
+  {
+    id: "3",
+    value: "Leave for stadium",
+  },
+];
